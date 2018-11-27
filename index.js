1a0ff39e941952737a10526c5dcf957004c01c4b
Kristian Trenskow <trenskow@me.com>
2018-11-27 18:39:36 +0100

    Added more info.

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ app.set('view engine', 'pug');
 
 app.use(require('morgan')('combined'));
 
-app.get('/.well-known/acme-challenge/ZKxmuyO6D8KQphgOR6kbyitKd2hVlFa_JyU1Gld6vFI', (req, res) => {
-	res.header('Content-Type', 'text/plain');
-	res.send('ZKxmuyO6D8KQphgOR6kbyitKd2hVlFa_JyU1Gld6vFI.L6gGKzYokAOkGX6skL8ZPVIyHqt-8VGhda9QfSkvQtk');
-});
-
 app.use(stylus.middleware({
 	src: __dirname + '/public',
 	compile: (str, path) => {
